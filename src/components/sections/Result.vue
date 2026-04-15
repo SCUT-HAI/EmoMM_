a5923a797b564ed9d8a5fab33f0defb2f1bec040
Yimeng Zhang <154990490+Carolllmmm@users.noreply.github.com>
2026-04-15 21:03:38 +0800

    Update Result.vue

--- src/components/sections/Result.vue
+++ src/components/sections/Result.vue
@@ -1,1 +1,20 @@
+<script setup>
+import result from '../mds/result.md';
+</script>
 
+<template>
+  <div>
+    <el-divider />
+
+    <el-row justify="center">
+        <el-col :xs="24" :sm="20" :md="16" :lg="12" :xl="12">
+            <result />
+        </el-col>
+    </el-row>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
